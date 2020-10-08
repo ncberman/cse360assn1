4e91e4f793dc5acde0708fa138ad73b61ee9379a
ncberman <72539181+ncberman@users.noreply.github.com>
2020-10-07 23:17:26 -0700

    v1.1
    added functionality to methods
    add()
    subtract()
    toString()
    getTotal()
    
    Now keeps history of transactions which can be gotten from toString().

--- cse360assignment01/src/cse360assignment01/AddingMachine.java
+++ cse360assignment01/src/cse360assignment01/AddingMachine.java
@@ -2,25 +2,32 @@ package cse360assignment01;
 
 public class AddingMachine {
 	private int total;
+	private String txHistory = "0";
 	  
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	  
 	public int getTotal () { // returns current total
-	    return 0;
+	    return total;
 	}
 	  
 	public void add (int value) { // add an integer to the total
+		txHistory += (" + " + value); // record transaction
+		total += value;
 	}
 
 	public void subtract (int value) { // remove an integer to the total
+		txHistory += (" - " + value); //  record transaction
+		total -= value;
 	}
 
-	public String toString () { // return nothing
-		return "";
+	public String toString () { // return transaction history
+		return txHistory;
 	}
 
 	public void clear() { // reset the total value
+		total = 0;
+		txHistory = "";
 	}
 }
