271fba21c1ac53829f4aa1cf42b69041425976b5
ncberman <72539181+ncberman@users.noreply.github.com>
2020-10-07 22:47:51 -0700

    v1.0
    base file w/ comments

---
+++ cse360assignment01/src/cse360assignment01/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment01;
+
+public class AddingMachine {
+	private int total;
+	  
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	  
+	public int getTotal () { // returns current total
+	    return 0;
+	}
+	  
+	public void add (int value) { // add an integer to the total
+	}
+
+	public void subtract (int value) { // remove an integer to the total
+	}
+
+	public String toString () { // return nothing
+		return "";
+	}
+
+	public void clear() { // reset the total value
+	}
+}
